7d35f981746544b804371ec3a2d71620e56f7757
KIMSEI1124 <workju1124@gmail.com>
2024-01-02 19:37:38 +0900

    Study(Ci_Cd): shell Script 작성

---
+++ backend/run.sh
@@ -0,0 +1,23 @@
+###
+# Run.sh
+###
+APP_NAME=app
+IMAGE=ci_cd_image
+
+./gradlew clean build
+
+# Application Stop
+echo "🚫 Stop Docker Container"
+docker rm -f $APP_NAME
+
+# Docker 기존 이미지 삭제 및 생성
+echo "🗑️ Remove Docker Container"
+docker image rm $IMAGE
+docker build . -t $IMAGE
+
+# Docker Run
+echo "🚀 Docker $APP_NAME Container Start!"
+docker run -d \
+--name $APP_NAME \
+-p 8080:8080 \
+$IMAGE
---
+++ backend/src/test/java/com/cicd/backend/AppTest.java
@@ -0,0 +1,16 @@
+package com.cicd.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void run() {
+        boolean b = true;
+        assertThat(b).isTrue();
+    }
+}
